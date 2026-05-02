f0421d1cbe28665f7cba373ccb3b201f69b7dbad
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2026-05-02 12:48:28 -0400

    Added nodemailer and created Transporter var

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 const PORT = 5000;
 
