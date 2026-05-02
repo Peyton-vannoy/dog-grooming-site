15456744a91d51c84b032a30e57555d95cd0c976
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2026-05-02 13:51:12 -0400

    styling email body

--- server/server.js
+++ server/server.js
@@ -26,6 +26,7 @@ app.get("/", (req, res) => {
 app.post("/api/contact", async (req, res) => {
   try {
     const { name, email, message } = req.body;
+    console.log("User email from form", email);
 
     if (!name || !email || !message) {
       return res.status(400).json({
@@ -34,13 +35,25 @@ app.post("/api/contact", async (req, res) => {
       });
     }
     const info = await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+      from: `"Dog Grooming Website" <${process.env.EMAIL_USER}>`,
       to: process.env.EMAIL_TO,
+      replyTo: email,
       subject: "New Booking Request",
       text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+      html: `
+      <html style=" background-color: #ffffff;max-width: 300px; margin: 0 auto;">
+      <body>
+      <div style="padding: 20px; font-family: Arial, sans-serif; background-color: #f5f5f5; border-radius: 10px; max-width: 300px; margin: 0 auto;">
+      <h2 style="color: rgb(4, 158, 247);">New Booking Request</h2>
+      <p><strong>Name:</strong> ${name}</p><br>
+      <p><strong>Email:</strong> ${email}</p><br>
+      <p><strong>Message:</strong></p><br>
+      <p>${message}</p>
+      </div>
+      </body>
+      </html>
+      `,
     });
-
-    console.log("Email sent:", info.response);
 
     res.status(200).json({
       success: true,
